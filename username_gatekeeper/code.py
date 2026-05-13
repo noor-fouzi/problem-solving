60f8bacc776460f7e52c69d2cd4f7f89f6c57566
Noor <nfmsw2020@gmail.com>
2026-05-13 06:50:00 +0300

    Username Validation Tests
    I added some tests to check the process of username validation.

--- username_gatekeeper/code.py
+++ username_gatekeeper/code.py
@@ -9,3 +9,12 @@ def validate_username(username):
         return True
     
     return False
+
+
+print("12345: ", validate_username("12345")) # True
+print("test.username: ", validate_username("test.username")) # False
+print("username: ", validate_username("username")) # True
+print("hello, world: ", validate_username("hello, world")) # False
+print("user0123: ", validate_username("user0123")) # True
+print("_user: ", validate_username("_user")) # False
+print("user0123456user: ", validate_username("user0123456user")) # True
