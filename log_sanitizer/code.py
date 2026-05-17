4a55394f17d7169e8b98073574c3ac3877a992c7
Noor <nfmsw2020@gmail.com>
2026-05-17 19:57:02 +0300

    Login Logs Filter
    I finished implementing the actual code.
    I breaked the code down into several coding blocks.
    It was so much fun!

--- log_sanitizer/code.py
+++ log_sanitizer/code.py
@@ -1,5 +1,43 @@
+import os, sys
+
+utilities_directory = os.path.abspath(
+    os.path.join(
+            os.path.dirname(__file__), "./utilities"
+    )
+)
+
+if utilities_directory not in sys.path:
+    sys.path.append(utilities_directory)
+
+
+from check_format import is_well_formatted
+from split_string import split_string
+
+
 def filter_logs(login_logs):
-    pass
+    
+    successful_attempts = []
+    failed_attempts = []
+
+    for log in login_logs:
+
+        if is_well_formatted(log):
+            splited = split_string(log, "|")
+
+            if splited[2] == "200":
+                successful_attempts.append(splited[1])
+
+            else:
+                failed_attempts.append(splited[1])
+
+        else:
+            continue
+
+    return dict(
+        secure_events = successful_attempts,
+        flagged_attempts = failed_attempts
+    )
+
 
 
 if __name__ == "__main__":
@@ -11,7 +49,7 @@ if __name__ == "__main__":
         "2026-05-17 05:04:11 | validUser123 | 200"
     ]
 
-    filter_logs(raw_logs)
+    print(filter_logs(raw_logs))
     # >> {
     #       'secure_events':['noorfouzi', 'validUser123'],
     #       'flagged_attempts': ['admin', 'hacker_joe']
