679021999b66a0f87b61d77f290ddc5fb67fbae9
Noor <nfmsw2020@gmail.com>
2026-05-17 18:43:01 +0300

    String Spliter Updated
    I used the clear spaces function on the splited string.

--- log_sanitizer/utilities/split_string.py
+++ log_sanitizer/utilities/split_string.py
@@ -1,10 +1,12 @@
+from clear_spaces import clear_spaces
+
 def split_string(string, split_char = ' '):
     splited_string = []
     holder = ""
     for character in range (len(string)):
 
         if string[character] == split_char or  character == len(string):
-            splited_string.append(holder)
+            splited_string.append(clear_spaces(holder))
             holder = ""
         else:
             holder += string[character]
