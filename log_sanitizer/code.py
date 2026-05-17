4dc114459e8d582fa70b6411f80433c8eb3607a9
Noor <nfmsw2020@gmail.com>
2026-05-18 00:08:40 +0300

    Log Filter Updated
    I used the space cleaner before checking the status code and appending the username into its corresponding list.
    After checking the status code, ,I made sure it's valid using my validate username function.

--- log_sanitizer/code.py
+++ log_sanitizer/code.py
@@ -6,12 +6,23 @@ utilities_directory = os.path.abspath(
     )
 )
 
+parent_dirctory = os.path.abspath( 
+    os.path.join( 
+        os.path.dirname(__file__), 
+        '..'
+    )
+)
+
+if parent_dirctory not in sys.path:
+    sys.path.append(parent_dirctory)
+
 if utilities_directory not in sys.path:
     sys.path.append(utilities_directory)
 
-
+from username_gatekeeper.code import validate_username
 from check_format import is_well_formatted
 from split_string import split_string
+from clear_spaces import clear_spaces
 
 
 def filter_logs(login_logs):
@@ -24,11 +35,13 @@ def filter_logs(login_logs):
         if is_well_formatted(log):
             splited = split_string(log, "|")
 
-            if splited[2] == "200":
-                successful_attempts.append(splited[1])
+            if clear_spaces(splited[2]) == "200":
+                username = clear_spaces(splited[1])
+                if validate_username(username):
+                    successful_attempts.append(username)
 
             else:
-                failed_attempts.append(splited[1])
+                failed_attempts.append(clear_spaces(splited[1]))
 
         else:
             continue
@@ -46,7 +59,8 @@ if __name__ == "__main__":
         "2026-05-17 05:01:45 | admin | 403",
         "MALFORMED_LINE_ERR_909",
         "2026-05-17 05:03:22 | hacker_joe | 403",
-        "2026-05-17 05:04:11 | validUser123 | 200"
+        "2026-05-17 05:04:11 | validUser123 | 200",
+        "| | 200"
     ]
 
     print(filter_logs(raw_logs))
