bfe73451d66721580fced60c85e230a3afc1ccfe
Noor <nfmsw2020@gmail.com>
2026-05-17 08:45:50 +0300

    Username Bulk Processor - Edited
    I fixed the issue with the empty list.
    instead of printing an error message, a clear structure was returned.
    Since python doesn't return an error message when a for loops on an empty list, I removed the if statement that checks if a list is empty or not.
    Also, since the counter for invalid usernames is redundant and using len() on invaid_usernames list could replace it, I removed it.

--- username_bulk_processor/code.py
+++ username_bulk_processor/code.py
@@ -17,28 +17,28 @@ from username_gatekeeper.code import validate_username
 def process_signups(usernames_list):
 
     valid_usernames_count = 0
-    invalid_usernames_count = 0
     invalid_usernames = []
 
-    if len(usernames_list) > 0:
-        for username in usernames_list:
-            if validate_username(username):
-                valid_usernames_count += 1
-            else:
-                invalid_usernames_count += 1
-                invalid_usernames.append(username)
-    else:
-        print("We can't process an empty list!")
+    
+    for username in usernames_list:
+        if validate_username(username):
+            valid_usernames_count += 1
+        else:
+            invalid_usernames.append(username)
 
     return dict(
         successful_count = valid_usernames_count,
-        failed_count = invalid_usernames_count,
+        failed_count = len(invalid_usernames),
         rejected_usernames =  invalid_usernames
     )
 
 if __name__ == "__main__":
     print(process_signups([])) 
-    # >> We can't process an empty list!
+    # >> {
+    #       'successful_count': 0,
+    #       'failed_count' : 0,
+    #       'invalid_usernames': []
+    # }
 
     print(process_signups(["AdMiN", "User", "Username", "user_name"]))
     # >> {
