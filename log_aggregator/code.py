f50d7f322933cec8c6e6a89b5b8d3cdb4464ff02
Noor <nfmsw2020@gmail.com>
2026-05-20 13:55:58 +0300

    Login Logs Aggregator
    This is the initial code.

--- log_aggregator/code.py
+++ log_aggregator/code.py
@@ -1,10 +1,43 @@
 def aggregate_logs(web_logs, mobile_logs):
-    pass
+    
+    aggregator = {}
 
+    for log in web_logs:
+        user = log.get("user")
+        if user not in aggregator:
+            aggregator.update({
+                user : dict(
+                    web_sessions = [],
+                    mobile_devices = [],
+                    total_successful_logins = 0
+                )
+            })
 
+        
+        aggregator[user]["web_sessions"].append(log.get("session_id"))
+        if log.get("status") == "success":
+            aggregator[user]["total_successful_logins"] += 1
+
+    for log in mobile_logs:
+        device, user, status_code = log
+        if user not in aggregator:
+            aggregator.update({
+                user: dict(
+                    web_sessions = [],
+                    mobile_devices = [],
+                    total_successful_logins = 0
+                )
+            })
+
+        aggregator[user]["mobile_devices"].append(device)
+        if status_code == 200:
+            aggregator[user]["total_successful_logins"] += 1
+
+    return aggregator
+        
 
 if __name__ == "__main__":
-    # Structure: Contains 'session_id', 'user', and 'status'
+
     web_logs = [
         {"session_id": "W-901", "user": "noorfouzi", "status": "success"},
         {"session_id": "W-902", "user": "admin", "status": "failed"},
@@ -14,7 +47,6 @@ if __name__ == "__main__":
         {"session_id": "W-906", "user": "validUser123", "status": "failed"},
     ]
 
-    # Structure: Position-dependent (device_id, user_string, numeric_status_code)
     mobile_logs = [
         ("M-44", "noorfouzi", 200),
         ("M-45", "guest_user", 401),
@@ -22,3 +54,5 @@ if __name__ == "__main__":
         ("M-47", "validUser123", 200),
         ("M-48", "guest_user", 401),
     ]
+
+    print(aggregate_logs(web_logs, mobile_logs))
