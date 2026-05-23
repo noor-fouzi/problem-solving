91df7bb8d4c56e48ccb2f0523c795a242394e83a
Noor <nfmsw2020@gmail.com>
2026-05-24 00:14:43 +0300

    Test Results Aggregator
    This is the actual code of the solution I came up with.

--- test_suite_aggregator/code.py
+++ test_suite_aggregator/code.py
@@ -1,5 +1,33 @@
 def aggregate_test_results(test_results):
-    pass
+    
+    aggregated_test_results = {}
+
+    for result in test_results:
+        component = result.get("component")
+        status = result.get("status")
+        if component not in aggregated_test_results:
+            aggregated_test_results.update({
+                component: dict(
+                    total_executed = 0,
+                    passed_count = 0,
+                    failed_count = 0,
+                    pass_persentage = 0
+            )})
+
+        aggregated_test_results[component]["total_executed"] += 1
+
+        if status == "PASS":
+            aggregated_test_results[component]["passed_count"] += 1
+
+
+        elif status == "FAIL":
+            aggregated_test_results[component]["failed_count"] += 1
+
+        total_executed = aggregated_test_results[component]["total_executed"]
+        passed_count = aggregated_test_results[component]["passed_count"]
+        aggregated_test_results[component]["pass_persentage"] = int(passed_count / total_executed * 100)
+
+    return aggregated_test_results
 
 
 if __name__ == "__main__":
@@ -12,3 +40,5 @@ if __name__ == "__main__":
         {"component": "Payment", "test_name": "Apple_Pay_Integration", "status": "PASS"},
         {"component": "Authentication", "test_name": "Session_Timeout_Test", "status": "FAIL"}
     ]
+
+    print(aggregate_test_results(raw_test_results))
