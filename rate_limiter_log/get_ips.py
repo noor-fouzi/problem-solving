dff13a15c7d05ce2ca6aca2f3b8170a6c6472024
Noor <nfmsw2020@gmail.com>
2026-06-01 17:10:05 +0300

    Get IPs List - Edited
    I changed the function to take the result of the IPs Hit Endpoints as a dictionary, and return a list of its keys.

--- rate_limiter_log/get_ips.py
+++ rate_limiter_log/get_ips.py
@@ -3,24 +3,14 @@ def get_ips(logs):
     ips = []
 
     for log in logs:
-        ip = log.get("ip")
-        if ip not in ips:
-            ips.append(ip)
+        ips.append(log)
 
     return ips
 
 if __name__ == "__main__":
 
-    gateway_logs = [
-        {"ip": "192.168.1.1", "endpoint": "/login", "status": "200"},
-        {"ip": "10.0.0.5", "endpoint": "/search", "status": "200"},
-        {"ip": "192.168.1.1", "endpoint": "/login", "status": "401"},
-        {"ip": "192.168.1.1", "endpoint": "/login", "status": "200"},
-        {"ip": "10.0.0.5", "endpoint": "/checkout", "status": "200"},
-        {"ip": "192.168.1.1", "endpoint": "/login", "status": "200"},
-        {"ip": "192.168.1.1", "endpoint": "/login", "status": "401"},
-        {"ip": "192.168.1.1", "endpoint": "/login", "status": "200"}, # 6th request for this IP on /login! Limit exceeded!
-        {"ip": "10.0.0.5", "endpoint": "/banned_admin_route", "status": "404"} # Missing endpoint from limits matrix!
-    ]
-
+    gateway_logs = {
+        '192.168.1.1': {'/login': 6}, 
+        '10.0.0.5': {'/search': 1, '/checkout': 1, '/banned_admin_route': 1}
+    }
     print(get_ips(gateway_logs))
